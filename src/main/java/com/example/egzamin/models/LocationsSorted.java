67739d775da2760d17750313c35671b2f3293ff0
Miku91 <krystian5162@op.pl>
2022-06-12 22:21:27 +0200

    Initial commit

---
+++ src/main/java/com/example/egzamin/models/LocationsSorted.java
@@ -0,0 +1,27 @@
+package com.example.egzamin.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Data
+@Entity
+public class LocationsSorted {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    private String Adres;
+
+    public LocationsSorted (String adres){
+        this.Adres = adres;
+    }
+
+}
---
+++ src/main/java/com/example/egzamin/repository/LocationsRepository.java
@@ -0,0 +1,10 @@
+package com.example.egzamin.repository;
+
+import com.example.egzamin.models.Locations;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocationsRepository  extends JpaRepository<Locations,Long> {
+
+    Locations findTopByOrderByIdDesc();
+
+}
